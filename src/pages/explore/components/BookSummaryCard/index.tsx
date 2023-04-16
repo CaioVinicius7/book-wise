09f906abb09c9c1ee0e443f406575551849b0c4c
Caio Vinícius <caio1525pereira@gmail.com>
2023-04-16 16:38:52 -0300

    feat: :lipstick: Variante do componente BookSummaryCard

--- src/pages/explore/components/BookSummaryCard/index.tsx
+++ src/pages/explore/components/BookSummaryCard/index.tsx
@@ -1,37 +1,81 @@
 import Image from "next/image";
 import { Rating } from "react-simple-star-rating";
+import { BookOpen, BookmarkSimple } from "phosphor-react";
 
 import type { BookSummary } from "../../index.page";
 
-import { BookSummaryCardContainer, BookSummaryCardContent } from "./styles";
+import {
+  BookSummaryCardContainer,
+  BookSummaryCardContent,
+  BookSummaryCardFooter
+} from "./styles";
 
-interface BookSummaryCardProps extends BookSummary {}
+interface BookSummaryCardProps extends BookSummary {
+  variant?: "list" | "sidebar";
+}
 
 export function BookSummaryCard({
   title,
   author,
   rating,
-  coverImageUrl
+  coverImageUrl,
+  variant = "list"
 }: BookSummaryCardProps) {
+  const isListVariant = variant === "list";
+
+  const imgSize = {
+    width: isListVariant ? 108 : 170,
+    height: isListVariant ? 152 : 242
+  };
   return (
-    <BookSummaryCardContainer>
-      <Image src={coverImageUrl} width={108} height={152} alt="Capa do livro" />
-
+    <BookSummaryCardContainer isListVariant={isListVariant}>
       <BookSummaryCardContent>
-        <header>
-          <strong>{title}</strong>
-          <span>{author}</span>
-        </header>
+        <Image
+          src={coverImageUrl}
+          width={imgSize.width}
+          height={imgSize.height}
+          alt="Capa do livro"
+        />
 
-        <footer>
-          <Rating
-            size={20}
-            fillColor="#8381D9"
-            initialValue={rating}
-            readonly
-          />
-        </footer>
+        <div>
+          <header>
+            <strong>{title}</strong>
+            <span>{author}</span>
+          </header>
+
+          <footer>
+            <Rating
+              size={20}
+              fillColor="#8381D9"
+              initialValue={rating}
+              readonly
+            />
+            {!isListVariant && <span>3 Avaliações</span>}
+          </footer>
+        </div>
       </BookSummaryCardContent>
+
+      {!isListVariant && (
+        <BookSummaryCardFooter>
+          <div>
+            <BookmarkSimple size={24} color="#50B2C0" />
+
+            <div>
+              <span>Categoria</span>
+              <strong>Computação</strong>
+            </div>
+          </div>
+
+          <div>
+            <BookOpen size={24} color="#50B2C0" />
+
+            <div>
+              <span>Páginas</span>
+              <strong>425</strong>
+            </div>
+          </div>
+        </BookSummaryCardFooter>
+      )}
     </BookSummaryCardContainer>
   );
 }
--- src/pages/explore/components/BookSummaryCard/styles.ts
+++ src/pages/explore/components/BookSummaryCard/styles.ts
@@ -5,27 +5,88 @@ export const BookSummaryCardContainer = styled("article", {
   backgroundColor: "$gray700",
   borderRadius: 8,
 
-  cursor: "pointer",
-
-  display: "flex",
-  gap: "$5"
+  variants: {
+    isListVariant: {
+      true: {
+        cursor: "pointer"
+      }
+    }
+  }
 });
 
 export const BookSummaryCardContent = styled("div", {
   display: "flex",
-  flexDirection: "column",
-  justifyContent: "space-between",
+  gap: "$5",
 
-  header: {
-    strong: {
-      lineHeight: "$base"
+  div: {
+    display: "flex",
+    flexDirection: "column",
+    justifyContent: "space-between",
+    header: {
+      strong: {
+        lineHeight: "$base"
+      },
+
+      span: {
+        display: "block",
+        fontSize: "$sm",
+        color: "$gray400",
+        lineHeight: "$base"
+      }
     },
 
-    span: {
-      display: "block",
-      fontSize: "$sm",
-      color: "$gray400",
-      lineHeight: "$base"
+    footer: {
+      span: {
+        color: "$gray400",
+        fontSize: "$sm",
+        lineHeight: "$short",
+
+        display: "block"
+      }
     }
+  },
+
+  "@media(max-width: 400px)": {
+    img: {
+      width: 108,
+      height: 152
+    }
+  }
+});
+
+export const BookSummaryCardFooter = styled("footer", {
+  marginTop: "$10",
+  padding: "$6 0 0 0",
+
+  borderTop: "1px solid $gray600",
+
+  display: "grid",
+  gridTemplateColumns: "repeat(2, 1fr)",
+  gap: "$4",
+
+  div: {
+    display: "flex",
+    alignItems: "center",
+    gap: "$5",
+
+    div: {
+      display: "unset",
+
+      span: {
+        fontSize: "$sm",
+        color: "$gray300",
+        lineHeight: "$short",
+
+        display: "block"
+      },
+      strong: {
+        color: "$gray200",
+        lineHeight: "$short"
+      }
+    }
+  },
+
+  "@media(max-width: 400px)": {
+    gridTemplateColumns: "1fr"
   }
 });
